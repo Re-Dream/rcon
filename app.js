22bceb8d89bb80c071e10b6578082640709c1c5e
Marceau Maubert <tenrys.iaido@live.fr>
2018-04-10 00:17:33 +0200

    Add Steam login

--- app.js
+++ app.js
@@ -1,38 +1,45 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors")
+var express = require("express")
+var path = require("path")
+var cookieParser = require("cookie-parser")
+var logger = require("morgan")
+var steam = require("steam-login")
 
-var indexRouter = require('./routes/index');
+var indexRouter = require("./routes/index")
 
-var app = express();
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"))
+app.set("view engine", "pug")
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger("dev"))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, "public")))
+app.use(require("express-session")({ resave: false, saveUninitialized: false, secret: "redream_rcon" }))
+app.use(steam.middleware({
+    realm: "http://localhost:3000/",
+    verify: "http://localhost:3000/verify",
+    apiKey: process.argv[2]}
+))
 
-app.use('/', indexRouter);
+app.use("/", indexRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-	next(createError(404));
-});
+	next(createError(404))
+})
 
 // error handler
 app.use(function(err, req, res, next) {
 	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+	res.locals.message = err.message
+	res.locals.error = req.app.get("env") === "development" ? err : {}
 
 	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
-});
+	res.status(err.status || 500)
+	res.render("error")
+})
 
-module.exports = app;
+module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
-var express = require('express');
-var router = express.Router();
+var express = require("express")
+var steam = require("steam-login")
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-	res.render('index', { title: 'Express' });
-});
 
-module.exports = router;
+function isAuthed(req, res, callback) {
+
+	if (req.user == null) {
+		res.redirect("/authenticate")
+	} else {
+		callback()
+	}
+}
+
+router.get("/", function(req, res) {
+	isAuthed(req, res, function() {
+		res.render("index", {
+			name: req.user.username
+		})
+	})
+})
+
+router.get("/authenticate", steam.authenticate(), function(req, res) {
+    res.redirect("/")
+})
+
+router.get("/verify", steam.verify(), function(req, res) {
+	res.redirect("/")
+	// res.send(req.user).end()
+})
+
+router.get("/logout", steam.enforceLogin("/"), function(req, res) {
+    req.logout()
+    res.redirect("/")
+})
+
+module.exports = router
